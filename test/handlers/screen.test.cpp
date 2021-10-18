b1b17e188124b421f97b32e26700d64d66c3400e
Calgary Michael <cseth.michael@gmail.com>
2021-10-17 22:24:10 -0700

    tests for generate_line and generate_lines

--- test/handlers/screen.test.cpp
+++ test/handlers/screen.test.cpp
@@ -4,6 +4,123 @@
 #include <barthes/handlers/input.h>
 #include <barthes/handlers/screen.h>
 
+
+TEST(ScreenHandlerTest, generate_line__empty__no_value) {
+    barthes::TermConfig tc = {
+        .window_size=std::make_pair(50, 50),
+        .view=std::make_pair(0, 0),
+        .cursor=std::make_pair(0, 0),
+        .file_buffer=std::vector<std::string>{
+            ""
+        }
+    };
+    std::string actual = barthes::generate_line(&tc, tc.file_buffer, 0);
+    std::string expected = "\n";
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(ScreenHandlerTest, generate_line__empty__outside_screen) {
+    barthes::TermConfig tc = {
+        .window_size=std::make_pair(50, 50),
+        .view=std::make_pair(0, 49),
+        .cursor=std::make_pair(0, 0),
+        .file_buffer=std::vector<std::string>{
+            "Miles Davis"
+        }
+    };
+    std::string actual = barthes::generate_line(&tc, tc.file_buffer, 0);
+    std::string expected = "\n";
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(ScreenHandlerTest, generate_line__in_view) {
+    barthes::TermConfig tc = {
+        .window_size=std::make_pair(50, 50),
+        .view=std::make_pair(0, 0),
+        .cursor=std::make_pair(0, 0),
+        .file_buffer=std::vector<std::string>{
+            "Miles Davis"
+        }
+    };
+    std::string actual = barthes::generate_line(&tc, tc.file_buffer, 0);
+    std::string expected = "Miles Davis\n";
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(ScreenHandlerTest, generate_line__in_view__partial__start) {
+    barthes::TermConfig tc = {
+        .window_size=std::make_pair(50, 50),
+        .view=std::make_pair(0, 4),
+        .cursor=std::make_pair(0, 0),
+        .file_buffer=std::vector<std::string>{
+            "Miles Davis"
+        }
+    };
+    std::string actual = barthes::generate_line(&tc, tc.file_buffer, 0);
+    std::string expected = "s Davis\n";
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(ScreenHandlerTest, generate_line__in_view__partial__end) {
+    barthes::TermConfig tc = {
+        .window_size=std::make_pair(50, 9),
+        .view=std::make_pair(0, 0),
+        .cursor=std::make_pair(0, 0),
+        .file_buffer=std::vector<std::string>{
+            "Miles Davis"
+        }
+    };
+    std::string actual = barthes::generate_line(&tc, tc.file_buffer, 0);
+    std::string expected = "Miles Da\n";
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(ScreenHandlerTest, generate_lines__in_view) {
+    barthes::TermConfig tc = {
+        .window_size=std::make_pair(50, 50),
+        .view=std::make_pair(0, 0),
+        .cursor=std::make_pair(0, 0),
+        .file_buffer=std::vector<std::string>{
+            "Miles Davis",
+            "Charles Mingus"
+        }
+    };
+    std::string actual = barthes::generate_lines(&tc, tc.file_buffer);
+    std::string expected = "Miles Davis\nCharles Mingus\n";
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(ScreenHandlerTest, generate_lines__in_view__partial__row) {
+    barthes::TermConfig tc = {
+        .window_size=std::make_pair(50, 50),
+        .view=std::make_pair(2, 0),
+        .cursor=std::make_pair(0, 0),
+        .file_buffer=std::vector<std::string>{
+            "Miles Davis",
+            "Charles Mingus",
+            "Nina Simone"
+        }
+    };
+    std::string actual = barthes::generate_lines(&tc, tc.file_buffer);
+    std::string expected = "Nina Simone\n";
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(ScreenHandlerTest, generate_lines__in_view__partial__col) {
+    barthes::TermConfig tc = {
+        .window_size=std::make_pair(50, 9),
+        .view=std::make_pair(0, 0),
+        .cursor=std::make_pair(0, 0),
+        .file_buffer=std::vector<std::string>{
+            "Miles Davis",
+            "Charles Mingus"
+        }
+    };
+    std::string actual = barthes::generate_lines(&tc, tc.file_buffer);
+    std::string expected = "Miles Da\nCharles \n";
+    EXPECT_EQ(actual, expected);
+}
+
 TEST(ScreenHandlerTest, adjust_view__no_adjustment) {
     barthes::TermConfig tc = {
         .window_size=std::make_pair(50, 50),
